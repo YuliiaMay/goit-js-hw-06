94b556a51ae24421f9bed6a5812e6a7f1fb47ebd
YuliiaMay <yuliiamaystrenko@gmail.com>
2023-01-10 21:54:23 +0200

    correction

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -6,46 +6,36 @@ const refs = {
   input: document.querySelector('input'),
   buttonCreate: document.querySelector('button[data-create]'),
   buttonDestroy: document.querySelector('button[data-destroy]'),
-  boxes: document.querySelector('#boxes')
+  boxes: document.querySelector('#boxes'), 
 }
 
-// console.log(refs.boxes);
-
+// стоврення елементів
 const createBoxes = (amount) => {
-  // const numberOfDiv = amount;
   const divArray = [];
 
 	for (let i = 0; i < amount; i += 1) {
     const divEl = document.createElement('div');
-
-    // console.log(divEl.innerHTML);
 
     divEl.style.height = `${30 + 10 * i}px`;
     divEl.style.width = `${30 + 10 * i}px`;
     divEl.style.background = getRandomHexColor();
     divArray.push(divEl);
     // refs.boxes.insertAdjacentHTML('beforeend', divEl)
-
 	}
   return divArray;
 }
 
-// console.log(divEl);
-console.log(createBoxes(2));
-
+// фукція створення елементів
 const onButtonCreateClick = () => {
-  // refs.boxes.textContent = refs.input.currentTarget.value;
-
-  let boxesSet = createBoxes(refs.input.value)
-	refs.boxes.append(...boxesSet)
-    // refs.boxes.insertAdjacentHTML('beforeend',  ...boxesSet)
-
+  let boxesSet = createBoxes(refs.input.value);
+  refs.boxes.append(...boxesSet);
 }
 
-const onButtonDestroyClick = (event) => {
-  event.currentTarget.reset();
-  // refs.boxes.innerHTML = '';
+// фукція видалення елементів
+const onButtonDestroyClick = () => {
+  refs.boxes.innerHTML = '';
 }
 
+// виклик подій
 refs.buttonCreate.addEventListener('click', onButtonCreateClick);
 refs.buttonDestroy.addEventListener('click', onButtonDestroyClick);
